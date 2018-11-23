bffdce77e3563bd7b8c3722ea37555ec2e504b3a
fourwinds04 <fourwinds04@users.noreply.github.com>
2018-11-23 22:47:32 +0100

    aggiunte cose

--- javascript/logic.js
+++ javascript/logic.js
@@ -1,2 +1,48 @@
 var nomeER = /^[a-z]+$/i
 var codFiscER = /^[A-Z]{6}\d{2}[A-Z]\d{2}[A-Z]\d{3}[A-Z]$/i
+var eMailER = /^.+[@]{1}.+[.]{1}.{2,3}/i
+var capER = /^\d{5}/i
+var telER = /^\d{8,}/i
+
+var utente = {nome: "Michele", cognome: "Di Lollo", mail: "micheledilollo@gmail.com", pass: "alemioamore"}
+
+
+function iscrizione() {
+    var nome = document.registrazione.nome.value
+    var cognome = document.registrazione.cognome.value
+    var mail = document.registrazione.mail.value
+    var password = document.registrazione.pass.value
+    var password2 = document.registrazione.pass2.value
+
+    if(nomeEr.test(nome)) {
+        if(cognomeEr.test(cognome)) {
+            if(eMailER.test(mail)){
+                if(password != ""){
+                    if(password == password2){
+                        if(localStorage.getItem("utenti") == null){
+
+                        }
+                        else{
+
+                        }
+                    }
+                    else {
+                        alert("La password di conferma non corrisponde con la password")
+                    }
+                }
+                else {
+                    alert("Inserire una password")
+                }
+            }
+            else {
+                alert("Formato eMail non corretto")
+            }
+        }
+        else {
+            alert("Formato Cognome non corretto")
+        }
+    }
+    else {
+        alert("Formato Nome non corretto")
+    }
+}
