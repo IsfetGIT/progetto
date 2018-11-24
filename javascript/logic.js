691faceabb9ac88ae08b44fb23a42c3a4df46bd1
fourwinds04 <fourwinds04@users.noreply.github.com>
2018-11-24 17:27:11 +0100

    sistemate cose
    ora funzionano i controlli

--- javascript/logic.js
+++ javascript/logic.js
@@ -1,24 +1,27 @@
-var nomeER = /^[a-z]+$/i
-var codFiscER = /^[A-Z]{6}\d{2}[A-Z]\d{2}[A-Z]\d{3}[A-Z]$/i
-var eMailER = /^.+[@]{1}.+[.]{1}.{2,3}/i
-var capER = /^\d{5}/i
-var telER = /^\d{8,}/i
+var nomeER = /^[a-z]+$/i ;
+var codFiscER = /^[A-Z]{6}\d{2}[A-Z]\d{2}[A-Z]\d{3}[A-Z]$/i ;
+var eMailER = /^.+[@]{1}.+[.]{1}.{2,3}/i ;
+var capER = /^\d{5}/i ;
+var telER = /^\d{8,}/i ;
 
-var utente = {nome: "Michele", cognome: "Di Lollo", mail: "micheledilollo@gmail.com", pass: "alemioamore"}
+var utente = {nome: "Michele", cognome: "Di Lollo", mail: "micheledilollo@gmail.com", pass: "alemioamore"};
 
 
 function iscrizione() {
-    var nome = document.registrazione.nome.value
-    var cognome = document.registrazione.cognome.value
-    var mail = document.registrazione.mail.value
-    var password = document.registrazione.pass.value
-    var password2 = document.registrazione.pass2.value
+    var nomeF = document.registrazione.nome.value ;
+    var cognomeF = document.registrazione.cognome.value ;
+    var mailF = document.registrazione.mail.value ;
+    var passwordF = document.registrazione.pass.value ;
+    var password2F = document.registrazione.pass2.value ;
 
-    if(nomeEr.test(nome)) {
-        if(cognomeEr.test(cognome)) {
-            if(eMailER.test(mail)){
-                if(password != ""){
-                    if(password == password2){
+    console.log(nomeF) ;
+
+    if(nomeER.test(nomeF)) {
+        if(nomeER.test(cognomeF)) {
+            if(eMailER.test(mailF)){
+                if(passwordF != ""){
+                    if(passwordF == password2F){
+                        var utente = {nome: nomeF , cognome: cognomeF, mail: mailF, password: passwordF};
                         if(localStorage.getItem("utenti") == null){
 
                         }
@@ -27,22 +30,22 @@ function iscrizione() {
                         }
                     }
                     else {
-                        alert("La password di conferma non corrisponde con la password")
+                        alert("La password di conferma non corrisponde con la password");
                     }
                 }
                 else {
-                    alert("Inserire una password")
+                    alert("Inserire una password");
                 }
             }
             else {
-                alert("Formato eMail non corretto")
+                alert("Formato eMail non corretto");
             }
         }
         else {
-            alert("Formato Cognome non corretto")
+            alert("Formato Cognome non corretto");
         }
     }
     else {
-        alert("Formato Nome non corretto")
+        alert("Formato Nome non corretto");
     }
 }
