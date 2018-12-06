cdb3dae2dd3a3e2d63925fb2df90d827d0bf0d10
root <root@localhost.localdomain>
2018-12-06 08:55:41 +0100

    nginx-nodeJS

--- webApp/server.js
+++ webApp/server.js
@@ -1,14 +1,47 @@
-"use strict"
+//Lets require/import the HTTP module
+var http = require('http');
+var req = require('request');
 
-const express = require("express");
+//Lets define a port we want to listen to
+const PORT=8081; 
 
-const port = 8080;
-const host = "0.0.0.0"
+//We need a function which handles requests and send response
 
-const app = express();
-app.get("/", (req, res) => {
-	res.send("Hello World\n");
+function handleRequest(request, response){
+    response.end('It Works!! Path Hit: ' + request.url);
+	console.log(request);
+	console.log(response);
+	contact_remote();
+}
+
+function contact_remote(){
+
+	req({
+    	url: 'http://localhost:5984', //URL to hit
+    	method: 'GET'
+	}, function(error, resp, body1){
+    	if(error) {
+        	console.log(error);
+    	} else {
+        	//console.log('-->'+resp.statusCode, body1);
+			console.log('**-->'+body1);
+    	}
+	});
+
+}
+
+
+//Create a server
+var server = http.createServer(handleRequest);
+
+//Lets start our server
+//Lets start our server
+server.listen(PORT, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT);
+
+
 });
 
-app.listen(PORT, HOST);
-console.log("Running on http://${HOST}:${PORT}"); 
+
+
