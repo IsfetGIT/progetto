c4ee966408d53f95ace937540670dcd0e25c3a2d
fourwinds04 <45125442+fourwinds04@users.noreply.github.com>
2018-12-18 13:28:06 +0100

    Aggiunto scheletro carrello
    agigunto lo scheletro della funzione che genera il carrello

--- javascript/carrello.js
+++ javascript/carrello.js
@@ -36,6 +36,62 @@ function aggCarrello(oggetto) {
 }
 
 function setCarrello() {
-    
+    var utenteCollJSON = sessionStorage.getItem("utenteColl");
+    if(utenteCollJSON == null){
+        alert("Devi prima accedere per visualizzare il carrello");
+        return;
+    }
+    var utenteColl = JSON.parse(utenteCollJSON);
+    var carrelloJSON = localStorage.getItem("carrello");
+    if(carrelloJSON == null){
+        "Non hai nessun elemento nel carrello";
+        return;
+    }
+    var carrello = JSON.parse(carrelloJSON);
+    for(i = 0; i < carrello.length ; i++){
+        if(carrello[i].mail == utenteColl.mail) {
+            var prezzoTot = 0;
+            for(t = 0; t< carrello[i].oggettiCarrello.length; t++){    
+                var oggettoC = carrello[i].oggettiCarrello[t];
+                var oggetto = oggettoC.valore;
+                var quantita = oggettoC.numero;
+                prezzoTot += oggetto.prezzo;
+                var trEl = document.createElement("tr");
+                var tdimgEl = document.createElement("td");
+                var imgEl = document.createElement("img");
+                imgEl.setAttribute("src", oggetto.img);
+                imgEl.setAttribute("class", "imgOggetti");
+                tdimgEl.appendChild(imgEl);
+                var tdNome = document.createElement("td");
+                var textNome = document.createTextNode(oggetto.nome);
+                tdNome.appendChild(textNome);
+                var tdNum = document.createElement("td");
+                var textNum = document.createTextNode(quantita);
+                tdNum.appendChild(textNum);
+                var tdPrezzo = document.createElement("td");
+                var textPrezzo = document.createTextNode(oggetto.prezzo);
+                tdPrezzo.appendChild(textPrezzo);
+                var tdButton = document.createElement("td");
+                var button = document.createElement("button");
+                button.setAttribute("value", JSON.stringify(oggetto));
+                button.setAttribute("onclick", "rimuovi(this.value)" );
+                var textButton = document.createTextNode("Rimuovi dal carrello");
+                button.appendChild(textButton);
+                tdButton.appendChild(button);
+
+                trEl.appendChild(tdimgEl);
+                trEl.appendChild(tdNome);
+                trEl.appendChild(tdNum);
+                trEl.appendChild(tdPrezzo);
+                trEl.appendChild(tdButton);
+                var tabella = document.getElementById("table");
+                tabella.appendChild(trEl);
+            }
+            "Stampa il prezzo totale";
+            return;
+        }
+    }
+    "Nessun elemento nel carrello";
+
     
 }
